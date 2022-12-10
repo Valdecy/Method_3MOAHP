ead9b8656a9f9c760852f85f076c803c192d4509
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-12-10 19:13:04 -0300

    Update src.py

--- method_3mo_ahp/algorithm/src.py
+++ method_3mo_ahp/algorithm/src.py
@@ -576,10 +576,11 @@ class load_3moahp():
     ################################################################################
     # Function: Rank Decending (Adapted from: https://stackoverflow.com/questions/39059371/can-numpys-argsort-give-equal-element-the-same-rank)
     def rank_descending(self, x):
+        x              = -x
         u, inv, counts = np.unique(x, return_inverse = True, return_counts = True)
         csum           = np.zeros_like(counts)
         csum[1:]       = counts[:-1].cumsum()
-        return abs(csum[inv] - np.argmax(csum[inv]))
+        return csum[inv]
     
     # Functions: Objective Function 0 - Consistency Ratio (MI) 
     def f0(self, variables):
